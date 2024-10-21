7c906f8937dea0f7c437bb46ff19e8f512e3f2e7
AtunN25 <franklin25.2001@gmail.com>
2024-10-21 00:18:53 -0500

    infinity carousel added

--- src/components/MyStack/OtherStack.tsx
+++ src/components/MyStack/OtherStack.tsx
@@ -1,11 +1,11 @@
 import SubTitle from '../SubTitles'
 import powerBi from '../../assets/powerbi.svg'
 import BoxStack from './BoxStack'
+import "./stack.css"
 
 import {
 
   mdiChartBarStacked,
-  //mdiArch,
   mdiUbuntu,
   mdiGit,
   mdiLanguageJava,
@@ -21,29 +21,45 @@ import {
     { icon: mdiUbuntu, subtitle: "Ubuntu" },
     { icon: mdiGit, subtitle: "Git" },
     { icon: mdiGithub, subtitle: "Github" },
+    { icon: powerBi, subtitle: "PowerBi", isImage: true },
     { icon: mdiLanguageJava, subtitle: "Java" },
     { icon: mdiLanguageCpp, subtitle: "C++" },
     { icon: mdiNodejs, subtitle: "NodeJs" },
     { icon: mdiMicrosoftExcel, subtitle: "Excel" },
+    
   ];
 
-  //const carruselStack = [...stackItems,...stackItems]
+  const carruselStack = [...stackItems,...stackItems]
 
-function OtherStack() {
-  return (
-    <div className='h-2/3 bg-stone-900 rounded-xl'>
-      <SubTitle subtitle="Other Stack" iconName={mdiChartBarStacked} />
-      <div className='flex flex-col items-center space-y-1'>
-        {stackItems.map((item, index) => (
-          <BoxStack key={index} icon={item.icon} subtitleIcon={item.subtitle}></BoxStack>
-        ))}
-        <div>
-          <img className='h-10 flex items-center w-full' src={powerBi} />
-            <p className='font-thin text-center'>PowerBi</p>
+  function OtherStack() {
+    return (
+      <div className="h-2/3 bg-stone-900 rounded-xl">
+        <SubTitle subtitle="Other Stack" iconName={mdiChartBarStacked} />
+        <div className="h-[calc(100%-50px)] overflow-hidden relative">
+          <div className="animate-scroll flex flex-col items-center space-y-1">
+            {carruselStack.map((item, index) => (
+              item.isImage ? ( 
+                <div key={index} className="flex flex-col items-center">
+                  <img
+                    className="h-10 w-full"
+                    src={item.icon} 
+                    alt={item.subtitle}
+                  />
+                  <p className="font-thin text-center">{item.subtitle}</p>
+                </div>
+              ) : (
+                <BoxStack
+                  key={index}
+                  icon={item.icon}
+                  subtitleIcon={item.subtitle}
+                />
+              )
+            ))}
           </div>
+          
         </div>
-    </div>
-  )
-}
-
+      </div>
+    );
+  }
+  
 export default OtherStack
